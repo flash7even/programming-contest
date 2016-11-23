4c3907003297490a93cc2a1d7a4fbc30279d859e
flash7even <tarangokhan77@gmail.com>
2016-11-23 22:24:43 +0600

    Last Update

---
+++ Template/flash7even.cpp
@@ -0,0 +1,158 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define nl puts ("")
+#define sp printf ( " " )
+#define phl printf ( "hello\n" )
+#define ff first
+#define ss second
+#define POPCOUNT __builtin_popcountll
+#define RIGHTMOST __builtin_ctzll
+#define LEFTMOST(x) (63-__builtin_clzll((x)))
+#define MP make_pair
+#define FOR(i,x,y) for(vlong i = (x) ; i <= (y) ; ++i)
+#define ROF(i,x,y) for(vlong i = (y) ; i >= (x) ; --i)
+#define CLR(x,y) memset(x,y,sizeof(x))
+#define UNIQUE(V) (V).erase(unique((V).begin(),(V).end()),(V).end())
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define NUMDIGIT(x,y) (((vlong)(log10((x))/log10((y))))+1)
+#define SQ(x) ((x)*(x))
+#define ABS(x) ((x)<0?-(x):(x))
+#define FABS(x) ((x)+eps<0?-(x):(x))
+#define ALL(x) (x).begin(),(x).end()
+#define LCM(x,y) (((x)/gcd((x),(y)))*(y))
+#define SZ(x) ((vlong)(x).size())
+#define NORM(x) if(x>=mod) x-=mod;if(x<0) x+=mod;
+#define MOD(x,y) (((x)*(y))%mod)
+#define ODD(x) (((x)&1)==0?(0):(1))
+#define Set(N,cur) N=(N|(1<<cur))
+#define Reset(N,cur) N=(N&(~(1<<cur)))
+#define Check(N,cur) (!((N&(1<<cur))==0))
+#define dbgA(A,i) debug("At pos: ",i," = ",A[i])
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+#ifdef forthright48
+     #include <ctime>
+     clock_t tStart = clock();
+     #define debug(args...) {dbg,args; cerr<<endl;}
+     #define timeStamp debug ("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+     #define bug printf("%d\n",__LINE__);
+
+#else
+     #define debug(args...)  // Just strip off all debug tokens
+     #define timeStamp
+#endif
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cerr<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+inline vlong gcd ( vlong a, vlong b ) {
+    a = ABS ( a ); b = ABS ( b );
+    while ( b ) { a = a % b; swap ( a, b ); } return a;
+}
+
+vlong ext_gcd ( vlong A, vlong B, vlong *X, vlong *Y ){
+    vlong x2, y2, x1, y1, x, y, r2, r1, q, r;
+    x2 = 1; y2 = 0; x1 = 0; y1 = 1;
+    for (r2 = A, r1 = B; r1 != 0; r2 = r1, r1 = r, x2 = x1, y2 = y1, x1 = x, y1 = y ) {
+        q = r2 / r1; r = r2 % r1;
+        x = x2 - (q * x1); y = y2 - (q * y1);
+    }
+    *X = x2; *Y = y2;
+    return r2;
+}
+
+inline vlong modInv ( vlong a, vlong m ) {
+    vlong x, y;
+    ext_gcd( a, m, &x, &y );
+    x %= m;
+    if ( x < 0 ) x += m;
+    return x;
+}
+
+inline vlong power ( vlong a, vlong p ) {
+    vlong res = 1, x = a;
+    while ( p ) {
+        if ( p & 1 ) res = ( res * x );
+        x = ( x * x ); p >>= 1;
+    }
+    return res;
+}
+
+inline vlong bigmod ( vlong a, vlong p, vlong m ) {
+    vlong res = 1 % m, x = a % m;
+    while ( p ) {
+        if ( p & 1 ) res = ( res * x ) % m;
+        x = ( x * x ) % m; p >>= 1;
+    }
+    return res;
+}
+
+inline int STRLEN(char *s){
+    int p = 0; while(s[p]) p++; return p;
+}
+
+inline LL SQRT(LL a){
+    LL v = sqrt(a);
+    if(SQ(v) == a) return v;
+    else if(SQ(v+1) == a) return v+1;
+    else if(SQ(v+2) == a) return v+2;
+    else return -1; /// a can no be written as p^2
+}
+
+#define LL long long
+#define LLU long long unsigned int
+typedef long long vlong;
+typedef unsigned long long uvlong;
+typedef pair < int, int > pii;
+typedef pair < vlong, vlong > pll;
+
+//int knightDir[8][2] = { {-2,1},{-1,2},{1,2},{2,1},{2,-1},{-1,-2},{1,-2},{-2,-1} };
+//int dir4[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+//int dir8[8][2] = {{-1,0},{0,1},{1,0},{0,-1},{-1,-1},{1,1},{1,-1},{-1,1}};
+
+const vlong inf = 2147383647;
+const vlong mod = 1000000007;
+const double pi = 2 * acos ( 0.0 );
+const double eps = 1e-9;
+
+///=========================  TEMPLATE ENDS HERE  ========================///
+///=======================================================================///
+
+const int Size = 200005;
+
+int N,M,K;
+int A[Size];
+
+void solve(){
+
+}
+
+int main () {
+
+    #ifdef forthright48
+    /// freopen ( "input.txt", "r", stdin );
+    /// freopen ( "output.txt", "w", stdout );
+    #endif // forthright48
+
+    /// ========= WARNING CHECK ========= ///
+    ///   1. Check Sample Test,Read Notes ///
+    ///   2. Check Corner Case When N<3   ///
+    ///   3. Check Array Size, Mod, LL    ///
+    ///   4. Check DP Memo Array Part     ///
+    ///===================================///
+
+    int nCase;
+    scanf("%d",&nCase);
+    for(int cs = 1;cs<=nCase;cs++){
+        scanf("%d",&N);
+
+    }
+    return 0;
+}
