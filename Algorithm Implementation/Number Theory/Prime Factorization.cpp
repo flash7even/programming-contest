c07366dc9bdb5df3b5085bbbfb595bc66b26517a
Tarango Khan <tarangokhan77@gmail.com>
2018-07-25 14:42:18 +0600

    Update Prime Factorization.cpp

--- Algorithm Implementation/Number Theory/Prime Factorization.cpp
+++ Algorithm Implementation/Number Theory/Prime Factorization.cpp
@@ -1,19 +1,19 @@
 vector<pii> primeFactorize(LL num){
-	vector<pii> pfactors;
-	FOR(i,0,SZ(prime)-1){
-        LL pf = prime[i];
-        if(pf*pf>num) break;
-        if(num%pf == 0){
-            int cnt = 0;
-            while(num%pf == 0){
-                num/=pf;
-                cnt++;
-            }
-            pfactors.pb(MP(pf, cnt));
+    vector<pii> pfactors;
+    FOR(i,0,SZ(prime)-1){
+    LL pf = prime[i];
+    if(pf*pf>num) break;
+    if(num%pf == 0){
+        int cnt = 0;
+        while(num%pf == 0){
+        num/=pf;
+        cnt++;
         }
-	}
-	if(num>1){
+        pfactors.pb(MP(pf, cnt));
+    }
+    }
+    if(num>1){
         pfactors.pb(MP(num, 1));
-	}
-	return pfactors;
+    }
+    return pfactors;
 }
