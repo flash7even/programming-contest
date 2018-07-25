a75e43a667a1850d234ef795f64ea66003c35cc3
Tarango Khan <tarangokhan77@gmail.com>
2018-07-25 14:48:46 +0600

    Update Euler Phi.cpp

--- Algorithm Implementation/Number Theory/Euler Phi.cpp
+++ Algorithm Implementation/Number Theory/Euler Phi.cpp
@@ -1,21 +1,17 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 #define Max 1000000
 int phi[Max];
 
 void euler_phi(){
-	phi[1] = 1;
-	for(int i = 2;i<Max;i++){
-		if(!phi[i]){
-			phi[i] = i-1;
-			for(int j = (i<<1);j<Max;j+=i){
-				if(!phi[j]){
-					phi[j] = j;
-				}
-				phi[j] = phi[j]/i*(i-1);
-			}
-		}
-	}
-
+    phi[1] = 1;
+    for(int i = 2;i<Max;i++){
+        if(!phi[i]){
+            phi[i] = i-1;
+            for(int j = (i<<1);j<Max;j+=i){
+                if(!phi[j]){
+                    phi[j] = j;
+                }
+                phi[j] = phi[j]/i*(i-1);
+            }
+        }
+    }
 }
