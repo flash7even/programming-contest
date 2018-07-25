72de61320ec7cee57058a03739447a6b0ae246a3
Tarango Khan <tarangokhan77@gmail.com>
2018-07-25 14:52:34 +0600

    Update Bionomial Coefficient.cpp

--- Algorithm Implementation/Dynamic Programming/Bionomial Coefficient.cpp
+++ Algorithm Implementation/Dynamic Programming/Bionomial Coefficient.cpp
@@ -1,19 +1,14 @@
-/// Include My Code Template
-#include <bits/stdc++.h>
-using namespace std;
-#define Size 1005
-
 int DP[Size][Size];
 
 void binomialCoeff() {
     CLR(DP,0);
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= min(i, k); j++) {
-			if (j == 0 || j == i) {
-				DP[i][j] = 1;
-			} else {
-				DP[i][j] = DP[i - 1][j - 1] + DP[i - 1][j];
-			}
-		}
-	}
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= min(i, k); j++) {
+            if (j == 0 || j == i) {
+                DP[i][j] = 1;
+            } else {
+                DP[i][j] = DP[i - 1][j - 1] + DP[i - 1][j];
+            }
+        }
+    }
 }
