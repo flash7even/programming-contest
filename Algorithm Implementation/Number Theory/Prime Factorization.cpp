943b81977793f286f0cbceb947fb15531f408236
Tarango Khan <tarangokhan77@gmail.com>
2018-11-29 02:16:36 +0600

    Update Prime Factorization.cpp

--- Algorithm Implementation/Number Theory/Prime Factorization.cpp
+++ Algorithm Implementation/Number Theory/Prime Factorization.cpp
@@ -1,19 +1,21 @@
-vector<pii> primeFactorize(LL num){
-    vector<pii> pfactors;
-    FOR(i,0,SZ(prime)-1){
-    LL pf = prime[i];
-    if(pf*pf>num) break;
-    if(num%pf == 0){
-        int cnt = 0;
-        while(num%pf == 0){
-        num/=pf;
-        cnt++;
+vector<int> prime;
+
+vector< pair<int,int> > primeFactorize(LL num){
+    vector< pair<int,int> > flist;
+    for(int i = 0;i<prime.size();i++){
+        LL pf = prime[i];
+        if(pf*pf>num) break;
+        if(num%pf == 0){ /// pf is a prime factor of num
+            int cnt = 0;
+            while(num%pf == 0){ /// Find how many times num get divided by pf
+                num/=pf;
+                cnt++;
+            }
+            flist.pb(make_pair(pf, cnt));
         }
-        pfactors.pb(MP(pf, cnt));
-    }
     }
     if(num>1){
-        pfactors.pb(MP(num, 1));
+        flist.pb(make_pair(num, 1));
     }
-    return pfactors;
+    return flist;
 }
